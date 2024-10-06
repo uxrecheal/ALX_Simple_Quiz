947b4a0ffb08dd8976aece901dfe098b577f9058
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-10-06 22:17:52 +0000

    final commit

--- .vscode/calculator.js
+++ .vscode/calculator.js
@@ -1,15 +1,13 @@
 function add(number1, number2) {
     return number1 + number2;
 }
-function substruct(number1, number2){
+function subtruct(number1, number2){
     return number1 - number2;
 }
-function mutiply (number1, number2){
+function multiply (number1, number2){
     return number1 * number2;
 }
-function modulo (number1, number2){
-    return number1 % number2;
-}
+
 function divide(number1, number2){
     return number1 / number2;
 }
@@ -20,21 +18,21 @@ document.getElementById('add').addEventListener('click', function() {
     const result = add(number1, number2);
     document.getElementById('calculation-result').textContent = result;
 });
-document.getElementById('substract').addEventListener('click', function() {
+document.getElementById('subtract').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
-    const result = add(number1, number2);
+    const result = subtruct(number1, number2);
     document.getElementById('calculation-result').textContent = result;
 });
 document.getElementById('multiply').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
-    const result = add(number1, number2);
+    const result = multiply(number1, number2);
     document.getElementById('calculation-result').textContent = result;
 });
 document.getElementById('divide').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
-    const result = add(number1, number2);
+    const result = divide(number1, number2);
     document.getElementById('calculation-result').textContent = result;
 });
